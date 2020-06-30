285ebc77251c001ca88bb263ddee276b0e9395d1
Anan P <hs1gab@gmail.com>
2020-06-30 15:30:02 +0700

    Update AdminModel.php

--- Nantaburi/mongodb/src/Models/AdminModel.php
+++ Nantaburi/mongodb/src/Models/AdminModel.php
@@ -1,9 +1,22 @@
 <?php
-/*
-* Class functions  
-* - add Gtrade customer user 
-* - qurey Gtrade customer         
-*/
+/* ******************* 
+* project Nandave
+* edit file composor.json 
+* Create by team Nandev: 
+*                       Anan P.
+*                       Suphachai.
+*                       Tanapath.
+*                       
+* config: composer.json 
+* 
+* add  : "psr-4": { 
+*     
+*              "Nandev\\" : "nandev/"
+*
+*        }
+* and run command below in  connamd line :
+* $ composer dumpautoload
+************************* */
 
 namespace Nandev\Models;
 
